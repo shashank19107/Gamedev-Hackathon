ab240692c6516052aadc6d6cbab089441656b3ff
Raghav Nakra <71371427+ragh-a-void@users.noreply.github.com>
2021-03-20 10:45:15 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -12,7 +12,6 @@ light_red = (200, 0, 0)
 blue = (0, 0, 255)
 red_highlight = (240, 50, 50, 100)
 
-
 main_menu_bg_img = pygame.image.load("background_main_menu.jpeg")
 game_bg_img = pygame.image.load("game_bg.png")
 number1_img = pygame.image.load("number1.png")
@@ -22,18 +21,20 @@ number4_img = pygame.image.load("number4.png")
 number5_img = pygame.image.load("number5.png")
 number6_img = pygame.image.load("number6.png")
 
-game_display = pygame.display.set_mode((1280, 720))
+game_display = pygame.display.set_mode((1920, 1080))
 pygame.display.update()
 
 
-def button(x_button, y_button, msg):
-    pygame.draw.rect(game_display, white, [x_button, y_button, 100, 30])
-    message(50, msg, x_button, y_button, black)
+def button(x_button, y_button, msg, color1, color2):
+    button_width = 150
+    button_height = 50
+    pygame.draw.rect(game_display, color1, [x_button, y_button, button_width, button_height])
+    message(100, msg, x_button, y_button, black)
     mouse_pos = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-    if x_button + 100 > mouse_pos[0] > x_button and y_button + 30 > mouse_pos[1] > y_button:
-        pygame.draw.rect(game_display, off_white, [x_button, y_button, 100, 30])
-        message(50, msg, x_button, y_button, black)
+    if x_button + button_width > mouse_pos[0] > x_button and y_button + button_height > mouse_pos[1] > y_button:
+        pygame.draw.rect(game_display, color2, [x_button, y_button, button_width, button_height])
+        message(100, msg, x_button, y_button, black)
         if click == (1, 0, 0) and msg == "PLAY":
             game_loop()
         elif click == (1, 0, 0) and msg == "QUIT":
@@ -47,8 +48,8 @@ def main_menu():
     game_play = False
     while not game_play:
         game_display.blit(main_menu_bg_img, (0, 0))
-        button(1120, 550, "PLAY")
-        button(1120, 630, "QUIT")
+        button(1700, 800, "PLAY", white, off_white)
+        button(1700, 950, "QUIT", white, off_white)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -66,15 +67,15 @@ def game_loop():
     game_over = False
     while not game_over:
         game_display.blit(game_bg_img, (0, 0))
-        button(300, 300, "BACK")
+        button(50, 50, "BACK", white, off_white)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        message(100, "HAND CRICKET", 500, 50, white)
+        # message(100, "HAND CRICKET", 500, 50, white)
         clock.tick(60)
         pygame.display.update()
-        
+
 
 main_menu()
 pygame.quit()
